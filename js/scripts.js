b110ebcbd3b4cafae5ea2b5980d285495ff87dce
Keith Stedman and Xi Xia <xi@>
2017-03-01 09:22:25 -0800

    made init objects and methods for each object to operate on and by

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
-//Objects - players, board, spaces, game
-function Player(xORo,name,spaces){
-  this.xORo = xORo;
-  this.name = name,
-  this.spaces = spaces[];
-}
-
-function Space(xCoordinate, yCoordinate, whoHere){
-  this.xCoordinate=xCoordinate;
-  this.yCoordinate=yCoordinate;
-  this.whoHere=whoHere;
-}
-
-function Board(spaces, threeInRow){
-  this.allSpaces = allSpaces[];
-  this.threeInRow = threeInRow;
-}
-
-function Game(players, turn, board){
-  this.players = players[];
-  this.turn=turn;
+//business logic
+function Game (board,players,turn) {
   this.board=board;
+  this.players=[];
+  this.turn=turn;
 }
-$(document).ready(function(){
 
-});
+Game.prototype.status = function () {
+  return status;
+};
+
+function Board (spaces) {
+  this.spaces=[];
+}
+
+Board.prototype.threeInRow = function () {
+  return win or draw;
+};
+
+Board.prototype.find = function () {
+  return space x y
+};
+
+function Space (x,y,contents) {
+  this.x= x;
+  this.y= y;
+  this.contents=contents;
+}
+
+Space.prototype.mark = function () {
+  return lets player mark a space;
+};
+
+Space.prototype.markedBy = function () {
+  return which player marked space;
+};
